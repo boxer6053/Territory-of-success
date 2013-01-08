86257b4a386424be07a56bfbf1cbc229ed162e06
Andriy novoselskiy <boxer6053@gmail.com>
2013-01-08 17:23:00 +0200

    qwert

---
+++ TerritoryOfSuccess/MSFirstNavigationController.h
@@ -0,0 +1,14 @@
+//
+//  MSFirstNavigationController.h
+//  TerritoryOfSuccess
+//
+//  Created by Matrix Soft on 08.01.13.
+//  Copyright (c) 2013 Matrix Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSFirstNavigationController : UINavigationController
+@property (weak, nonatomic) IBOutlet UITabBarItem *myTabBarItem;
+
+@end
---
+++ TerritoryOfSuccess/MSFirstNavigationController.m
@@ -0,0 +1,39 @@
+//
+//  MSFirstNavigationController.m
+//  TerritoryOfSuccess
+//
+//  Created by Matrix Soft on 08.01.13.
+//  Copyright (c) 2013 Matrix Soft. All rights reserved.
+//
+
+#import "MSFirstNavigationController.h"
+
+@interface MSFirstNavigationController ()
+
+@end
+
+@implementation MSFirstNavigationController
+
+@synthesize myTabBarItem = _myTabBarItem;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
