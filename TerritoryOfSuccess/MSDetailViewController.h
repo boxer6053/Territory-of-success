0487ed4908a8d12a3decb8c21c80378d4453627d
matrixsoft <macbook@MacBook-macbook.local>
2013-01-17 15:17:35 +0200

    22

---
+++ TerritoryOfSuccess/MSDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSDetailViewController.h
+//  TerritoryOfSuccess
+//
+//  Created by matrixsoft on 17.01.13.
+//  Copyright (c) 2013 Matrix Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSDetailViewController : UIViewController
+
+@end
---
+++ TerritoryOfSuccess/MSDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  MSDetailViewController.m
+//  TerritoryOfSuccess
+//
+//  Created by matrixsoft on 17.01.13.
+//  Copyright (c) 2013 Matrix Soft. All rights reserved.
+//
+
+#import "MSDetailViewController.h"
+
+@interface MSDetailViewController ()
+
+@end
+
+@implementation MSDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
