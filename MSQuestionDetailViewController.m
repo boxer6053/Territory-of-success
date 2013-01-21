3a9fdefdffdf0edda4280d0d07b2e06b07a76ff5
Matrix Soft <matrixsoft@Matrixs-MacBook-Pro.local>
2013-01-21 13:57:59 +0200

    moore

---
+++ MSQuestionDetailViewController.m
@@ -0,0 +1,61 @@
+//
+//  MSQuestionDetailViewController.m
+//  TerritoryOfSuccess
+//
+//  Created by Matrix Soft on 1/10/13.
+//  Copyright (c) 2013 Matrix Soft. All rights reserved.
+//
+
+#import "MSQuestionDetailViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface MSQuestionDetailViewController ()
+
+@end
+
+@implementation MSQuestionDetailViewController
+@synthesize data;
+@synthesize questionDescription;
+@synthesize questionTitle;
+@synthesize questionDescriptionLabel;
+@synthesize questionTitleLabel;
+@synthesize tableOfAnswers;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableOfAnswers.layer.cornerRadius = 10;
+    [self.tableOfAnswers.layer setBorderColor:[UIColor colorWithRed:200/255.0 green:200/255.0 blue:200/255.0 alpha:1.0].CGColor];
+    [self.tableOfAnswers.layer setBorderWidth:1.0f];
+    [self.tableOfAnswers.layer setBackgroundColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.7].CGColor];
+    if ([[UIScreen mainScreen] bounds].size.height == 568) {
+        [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background_320*568.png"]]];
+    }
+    else
+    {
+        [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background_320*480.png"]]];
+    }
+    self.questionDescriptionLabel.text = questionDescription;
+    self.questionTitleLabel.text = questionTitle;
+    for (id key in [data allKeys])
+        NSLog(@"Key : %@ => value : %@", key, [data objectForKey:key]);
+    NSLog(@"Number %u", data.count);
+    
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- MSQuestionsViewController.m
+++ MSQuestionsViewController.m
@@ -34,13 +34,7 @@
     self.allQuestionsMode = YES;
     self.myQuestionsMode = NO;
     self.tableView.delegate = self;
-    self.tableView.dataSource = self;    
-    
-    self.tableOfAnswers.layer.cornerRadius = 10;
-    [self.tableOfAnswers.layer setBorderColor:[UIColor colorWithRed:200/255.0 green:200/255.0 blue:200/255.0 alpha:1.0].CGColor];
-    [self.tableOfAnswers.layer setBorderWidth:1.0f];
-    [self.tableOfAnswers.layer setBackgroundColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.7].CGColor];
-    
+    self.tableView.dataSource = self;
     self.tableView.layer.cornerRadius = 10;
     [self.tableView.layer setBorderColor:[UIColor colorWithRed:200/255.0 green:200/255.0 blue:200/255.0 alpha:1.0].CGColor];
     [self.tableView.layer setBorderWidth:1.0f];
@@ -49,19 +43,19 @@
     
     [self createAllArray];
     [self createMyArray];
- 
+    
     [super viewDidLoad];
     
     // [newDictionary setObject:@"You" forKey:@"title"];
     //
-    NSArray *questionTitles = [[NSArray alloc] initWithObjects:@"World", @"Name", @"You", nil];
-    NSArray *questionsDetails = [[NSArray alloc] initWithObjects:@"What is the world?", @"What is your name", @"who are you?", nil];
+    NSArray *questionTitles = [[NSArray alloc] initWithObjects:@"Вопрос 1", @"Вопрос 2", @"Вопрос 3", nil];
+    NSArray *questionsDetails = [[NSArray alloc] initWithObjects:@"Описание вопроса 1", @"Описание вопроса 2", @"Описание вопроса 3", nil];
     
     for(int i=0;i<questionsDetails.count;i++)
     {
         self.questionsDictionary = [[NSDictionary alloc] initWithObjects:questionsDetails forKeys:questionTitles];
         ////
-       
+        
         //
         [self.testDictionary setObject:[questionTitles objectAtIndex:i] forKey:@"Titles"];
         [self.testDictionary setObject:[questionsDetails objectAtIndex:i] forKey:@"Description"];
@@ -75,10 +69,10 @@
     //    self.testDictionary = [NSDictionary dictionaryWithObject:questionsDetails forKey:questionTitles];
     for (id key in [self.questionsDictionary allKeys])
     {
-
+        
         NSArray *array = [[NSMutableArray alloc] init];
         array = [self.questionsDictionary allKeys];
-
+        
         
         
         
@@ -89,21 +83,21 @@
 -(void)viewDidAppear:(BOOL)animated
 {
     if(allQuestionsMode){
-
+        
         UIColor *selectedColor=[UIColor colorWithRed:255.0/255.0 green:140.0/255.0 blue:0.0/255.0 alpha:1.0];
         [[self.segment.subviews objectAtIndex:1] setTintColor:selectedColor];
         [[self.segment.subviews objectAtIndex:0] setTintColor:[UIColor blackColor]];
-
+        
     }
     if(myQuestionsMode)
     {
         UIColor *selectedColor=[UIColor colorWithRed:255.0/255.0 green:140.0/255.0 blue:0.0/255.0 alpha:1.0];
         [[self.segment.subviews objectAtIndex:0] setTintColor:selectedColor];
         [[self.segment.subviews objectAtIndex:1] setTintColor:[UIColor blackColor]];
-
- 
+        
+        
     }
-       
+    
     
 }
 
@@ -115,7 +109,7 @@
 
 -(void)createAllArray
 {
-    NSArray *dataArray = [[NSArray alloc] initWithObjects:@"AllQuestion1",@"AllQuestion2", nil];
+    NSArray *dataArray = [[NSArray alloc] initWithObjects:@"Обший вопрос 1",@"Общий вопрос 2", nil];
     allArray = dataArray;
 }
 
@@ -125,151 +119,128 @@
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     if(tableView == _tableView){
-    return 1;
+        return 1;
     }
-else{
-    return 5;
-}
+    else{
+        return 5;
+    }
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-     
-    if(tableView ==_tableView){
+    
+    
     if(self.myQuestionsMode)
     {
-       
+        
         return self.questionsDictionary.count;
     }
     else{
         return myArray.count;
     }
-        
-    }
-else{
-    return 5;
+    
 }
-}
+
+
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell;
     if (tableView == _tableView)
-        {
-            static NSString* cellIdentifier = @"cellID";
+    {
+        static NSString* cellIdentifier = @"cellID";
         cell = [_tableView dequeueReusableCellWithIdentifier:cellIdentifier];
         if (cell == nil) {
             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
         }
-        //cell.imageView.image = [UIImage imageNamed:@"photo_camera_1.png"];
-//        cell.textLabel.text = @"Название категории";
-//        cell.detailTextLabel.text = @"Описание";
-
-//    
-//    static NSString *CellIdentifier = @"QuestionCellIdentifier";
-//    MSQuestionCell *cell1 = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-//    //    if (cell1 == nil) {
-//    //        cell1 = [[MSQuestionCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-//    //    }
-//    if(!cell1)
-//    {
-//        NSArray *topLevelObjects = [[NSBundle mainBundle] loadNibNamed:@"MSQuestionCell" owner:nil options:nil];
-//        for(id currentObject in topLevelObjects)
-//        {
-//            if([currentObject isKindOfClass:[MSQuestionCell class]])
-//            {
-//                cell1 = (MSQuestionCell *)currentObject;
-//                break;
-//            }
-//        }
-//    }
-//    
-//    //
-//    //         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-//    //         if (cell == nil) {
-//    //             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-//    //         }
-    if(self.myQuestionsMode)
-    {
-        int index = [indexPath indexAtPosition:1];
-        NSString *key = [[self.questionsDictionary allKeys] objectAtIndex:index];
-       // NSString *value = [self.questionsDictionary objectForKey:key];
-        cell.textLabel.text =key;
-        cell.detailTextLabel.text = @"grade";
-        //        for(int i=0;i<self.questionsDictionary.count;i++)
+        
+        
+        //
+        //    static NSString *CellIdentifier = @"QuestionCellIdentifier";
+        //    MSQuestionCell *cell1 = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        //    //    if (cell1 == nil) {
+        //    //        cell1 = [[MSQuestionCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+        //    //    }
+        //    if(!cell1)
+        //    {
+        //        NSArray *topLevelObjects = [[NSBundle mainBundle] loadNibNamed:@"MSQuestionCell" owner:nil options:nil];
+        //        for(id currentObject in topLevelObjects)
         //        {
-        //    cell.textLabel.text = [myArray objectAtIndex:indexPath.row]  ;
+        //            if([currentObject isKindOfClass:[MSQuestionCell class]])
+        //            {
+        //                cell1 = (MSQuestionCell *)currentObject;
+        //                break;
+        //            }
         //        }
+        //    }
+        //
+        //    //
+        //    //         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        //    //         if (cell == nil) {
+        //    //             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+        //    //         }
+        if(self.myQuestionsMode)
+        {
+            int index = [indexPath indexAtPosition:1];
+            NSString *key = [[self.questionsDictionary allKeys] objectAtIndex:index];
+            // NSString *value = [self.questionsDictionary objectForKey:key];
+            cell.textLabel.text =key;
+            cell.detailTextLabel.text = @"Оценка";
+            //        for(int i=0;i<self.questionsDictionary.count;i++)
+            //        {
+            //    cell.textLabel.text = [myArray objectAtIndex:indexPath.row]  ;
+            //        }
+        }
+        if(self.allQuestionsMode)
+        {
+            cell.textLabel.text = [allArray objectAtIndex:indexPath.row]  ;
+            cell.detailTextLabel.text = @"Оценка";
+        }
+        
+        
+        
     }
-    if(self.allQuestionsMode)
-    {
-        cell.textLabel.text = [allArray objectAtIndex:indexPath.row]  ;
-        cell.detailTextLabel.text = @"grade";
-    }
-    
-   
-    
-    }
-     return cell;
+    return cell;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if(tableView == _tableView)
     {
-    if(self.myQuestionsMode){
-        int index = [indexPath indexAtPosition:1];
-        NSString *key = [[self.questionsDictionary allKeys] objectAtIndex:index];
-        
-        self.questionTitle = key;
-        self.questionDescription = [self.questionsDictionary objectForKey:key];
-        
-        self.questionDetailDescriptionLabel.text = [self.questionsDictionary objectForKey:key];
-        self.questionDetailTitleLabel.text = key;
-      
-    }
-    if(self.allQuestionsMode)
-    {
-        
-        self.questionTitle = [allArray objectAtIndex:indexPath.row];
-           
-        self.questionDetailTitleLabel.text = [allArray objectAtIndex:indexPath.row];
-
-        self.questionDetailDescriptionLabel.text = @"description of the 'all question'";
-    }
-    //UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
-        [UIView animateWithDuration:0.5 animations:^{
-            self.segment.frame = CGRectMake(-340, self.segment.frame.origin.y, self.segment.frame.size.width,self.segment.frame.size.height);
-            self.tableView.frame = CGRectMake(-310, self.tableView.frame.origin.y, self.tableView.frame.size.width, self.tableView.frame.size.height);
-            self.tableOfAnswers.frame = CGRectMake(22, self.tableOfAnswers.frame.origin.y, self.tableOfAnswers.frame.size.width, self.tableOfAnswers.frame.size.height);
-            self.questionDetailTitleLabel.frame = CGRectMake(122, self.questionDetailTitleLabel.frame.origin.y, self.questionDetailTitleLabel.frame.size.width, self.questionDetailTitleLabel.frame.size.height);
-            self.questionDetailDescriptionLabel.frame = CGRectMake(20, self.questionDetailDescriptionLabel.frame.origin.y, self.questionDetailDescriptionLabel.frame.size.width, self.questionDetailDescriptionLabel.frame.size.height);
-            self.backButton.frame = CGRectMake(0, self.backButton.frame.origin.y, self.backButton.frame.size.width, self.backButton.frame.size.height);
+        if(self.myQuestionsMode){
+            int index = [indexPath indexAtPosition:1];
+            NSString *key = [[self.questionsDictionary allKeys] objectAtIndex:index];
             
-        } completion:^(BOOL finished) {
-            [UIView animateWithDuration:0.5 animations:^{
-                self.backButton.alpha = 1.0;
-            }];
-        }];
-//  
-//    [self performSegueWithIdentifier:@"toQuestionDetail" sender:self];
+            self.questionTitle = key;
+            self.questionDescription = [self.questionsDictionary objectForKey:key];
+            
+        }
+        if(self.allQuestionsMode)
+        {
+            
+            self.questionTitle = [allArray objectAtIndex:indexPath.row];
+            
+            
+        }
+        [self performSegueWithIdentifier:@"toQuestionDetail" sender:self];
     }
 }
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-//    if([segue.identifier isEqualToString:@"toQuestionDetail"]){
-//            MSQuestionDetailViewController *controller = (MSQuestionDetailViewController *)segue.destinationViewController;
-//        if(myQuestionsMode){
-//
-//        controller.data = self.questionsDictionary;
-//        controller.questionDescription = self.questionDescription;
-//        controller.questionTitle = self.questionTitle;
-//        }
-//        if(allQuestionsMode)
-//        {
-//            controller.questionDescription = @"All question description";
-//            controller.questionTitle = @" All question title";
-//        }
-//            
-//    }
+    if([segue.identifier isEqualToString:@"toQuestionDetail"]){
+        MSQuestionDetailViewController *controller = (MSQuestionDetailViewController *)segue.destinationViewController;
+        if(myQuestionsMode){
+            
+            controller.data = self.questionsDictionary;
+            controller.questionDescription = self.questionDescription;
+            controller.questionTitle = self.questionTitle;
+        }
+        if(allQuestionsMode)
+        {
+            controller.questionDescription = @"Описание общего вопроса";
+            //controller.questionTitle = @"Общий вопрос";
+            controller.questionTitle = self.questionTitle;
+        }
+        
+    }
 }
 
 
@@ -303,29 +274,15 @@ else{
         self.myQuestionsMode = NO;
         [self.tableView reloadData];
         
-    
+        
     }
     if(selectedSegment ==1)
     {
         self.myQuestionsMode = YES;
         self.allQuestionsMode = NO;
         [self.tableView reloadData];
-       
+        
     }
 }
 
-- (IBAction)backToQuestionTable:(id)sender {
-    [UIView animateWithDuration:0.2 animations:^{
-        self.backButton.alpha = 0.0;
-    } completion:^(BOOL finished) {
-    [UIView animateWithDuration:0.5 animations:^{
-        self.segment.frame = CGRectMake(-6, self.segment.frame.origin.y, self.segment.frame.size.width,self.segment.frame.size.height);
-        self.tableView.frame = CGRectMake(20, self.tableView.frame.origin.y, self.tableView.frame.size.width, self.tableView.frame.size.height);
-        self.tableOfAnswers.frame = CGRectMake(22+320, self.tableOfAnswers.frame.origin.y, self.tableOfAnswers.frame.size.width, self.tableOfAnswers.frame.size.height);
-        self.questionDetailTitleLabel.frame = CGRectMake(122+320, self.questionDetailTitleLabel.frame.origin.y, self.questionDetailTitleLabel.frame.size.width, self.questionDetailTitleLabel.frame.size.height);
-        self.questionDetailDescriptionLabel.frame = CGRectMake(20+320, self.questionDetailDescriptionLabel.frame.origin.y, self.questionDetailDescriptionLabel.frame.size.width, self.questionDetailDescriptionLabel.frame.size.height);
-        self.backButton.frame = CGRectMake(0+320, self.backButton.frame.origin.y, self.backButton.frame.size.width, self.backButton.frame.size.height);
-    }];
-  }];
-}
 @end
