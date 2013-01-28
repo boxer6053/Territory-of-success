60127d62140227c870814ecb7bc45d79c75bd3b4
Matrix Soft <matrixsoft@Matrixs-MacBook-Pro.local>
2013-01-28 10:59:30 +0200

    za

--- TerritoryOfSuccess/MSTypesOfInquirersViewController.m
+++ TerritoryOfSuccess/MSTypesOfInquirersViewController.m
@@ -17,14 +17,6 @@
 @synthesize whitchIsBetterButton     = _whitchIsBetterButton;
 @synthesize gradeButton = _gradeButton;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
