9df8b33687878adba111b489048d07acc6c03b6c
Matrix Soft <matrixsoft@Matrixs-MacBook-Pro.local>
2013-02-05 19:23:23 +0200

    questions

--- TerritoryOfSuccess/MSCreateQuestionViewController.h
+++ TerritoryOfSuccess/MSCreateQuestionViewController.h
@@ -1,19 +1,20 @@
-//
-//  MSCreateQuestionViewController.h
-//  TerritoryOfSuccess
-//
-//  Created by Matrix Soft on 1/31/13.
-//  Copyright (c) 2013 Matrix Soft. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface MSCreateQuestionViewController : UIViewController
+@protocol AddingQuestionDelegate <NSObject>
+
+-(void) addNewQuestion:(NSArray *) array;
+
+@end
+@interface MSCreateQuestionViewController : UIViewController <UITextViewDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *productImage;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 - (IBAction)askQuestionButton:(id)sender;
 @property (weak, nonatomic) NSString *productTitle;
 @property (weak, nonatomic) NSURL *imageURL;
+@property (strong, nonatomic) UITapGestureRecognizer *tap;
+@property (strong, nonatomic) id <AddingQuestionDelegate> delegate;
+@property (strong, nonatomic) NSMutableArray *sentArray;
+- (IBAction)cancelAction:(id)sender;
 
 @end
