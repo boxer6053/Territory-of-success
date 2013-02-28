41aaf8050b7632f0ae8327d4ca7573f0133c61db
Andriy novoselskiy <boxer6053@gmail.com>
2013-02-28 15:59:28 +0200

    added PrettyKit and customized navigation bar

--- TerritoryOfSuccess/MSFirstViewController.m
+++ TerritoryOfSuccess/MSFirstViewController.m
@@ -13,6 +13,7 @@
 #import "SVProgressHUD.h"
 #import <SDWebImage/UIButton+WebCache.h>
 #import <SDWebImage/UIImageView+WebCache.h>
+#import "PrettyKit.h"
 
 #define I 8
 
@@ -117,6 +118,8 @@
 
 - (void)viewDidLoad
 {
+    [self customizeNavBar];
+    
     [super viewDidLoad];
     
     self.beginCount = 0;
@@ -1234,6 +1237,24 @@ static inline double radians (double degrees)
         [self.profileBarButton setImage:nil];
         [self.profileBarButton setTitle:NSLocalizedString(@"Войти",nil)];
     }
+}
+
+- (void)customizeNavBar {
+    PrettyNavigationBar *navBar = (PrettyNavigationBar *)self.navigationController.navigationBar;
+//
+//    navBar.topLineColor = [UIColor colorWithHex:0xFF1000];
+//    navBar.gradientStartColor = [UIColor colorWithHex:0xDD0000];
+//    navBar.gradientEndColor = [UIColor colorWithHex:0xAA0000];
+//    navBar.bottomLineColor = [UIColor colorWithHex:0x990000];
+//    navBar.tintColor = navBar.gradientEndColor;
+    
+    navBar.topLineColor = [UIColor colorWithHex:0x676767];
+    navBar.gradientStartColor = [UIColor colorWithHex:0x373737];
+    navBar.gradientEndColor = [UIColor colorWithHex:0x1a1a1a];
+    navBar.bottomLineColor = [UIColor colorWithHex:0x000000];
+    navBar.tintColor = navBar.gradientEndColor;
+    
+//    navBar.roundedCornerRadius = 8;
 }
 
 @end
